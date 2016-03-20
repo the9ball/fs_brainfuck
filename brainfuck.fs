7bd89eb159c9284deea5d79d6225f5e9fb7bb8c0
Shaula <the9ball@gmail.com>
2016-03-20 19:58:26 +0900

    処理を再帰なシーケンシャルな処理に変えている途中(ビルド通らない)

--- brainfuck.fs
+++ brainfuck.fs
@@ -18,13 +18,13 @@ type Buffer(prev : Buffer option) =
 
     member this.Decr() = decr value
 
+    member this.IsZero() = 0 = (!value)
+
     member this.GetValue() = !value
 
     member this.GetChar() = Convert.ToChar !value
 
 let mutable currentBuffer = new Buffer(None)
-let mutable text = ""
-let mutable idx = 0
 
 let ReadFile filename =
     let file =
@@ -32,47 +32,54 @@ let ReadFile filename =
         |> Array.reduce (fun x y -> x + y)
     file.Replace ("\n", "")
 
-let gotoOpen () =
+let gotoOpen (text : string, _idx : int) =
+    let mutable idx = _idx
     let mutable nest = 1
     while 0 < nest && 0 <= idx do
         idx <- idx - 1
         match text.[idx] with
-        | '[' ->
-            nest <- nest - 1
-        | ']' ->
-            nest <- nest + 1
+        | '[' -> nest <- nest - 1
+        | ']' -> nest <- nest + 1
         | _ -> ()
+    idx
 
-let gotoClose () =
+let gotoClose (text : string, _idx : int) =
+    let mutable idx = _idx
     let mutable nest = 1
     while 0 < nest && idx <= text.Length do
         idx <- idx + 1
         match text.[idx] with
-        | '[' ->
-            nest <- nest + 1
-        | ']' ->
-            nest <- nest - 1
+        | '[' -> nest <- nest + 1
+        | ']' -> nest <- nest - 1
         | _ -> ()
+    idx
 
-let exec command =
+let execCore (command) =
     match command with
     | '+' -> currentBuffer.Incr()
     | '-' -> currentBuffer.Decr()
     | '<' -> currentBuffer <- currentBuffer.GetPrev().Value // 今回はoption型はずす
     | '>' -> currentBuffer <- currentBuffer.GetNext().Value // 今回はoption型はずす
-    | '[' -> if (currentBuffer.GetValue() = 0) then gotoClose()
-    | ']' -> if (currentBuffer.GetValue() <> 0) then gotoOpen()
     | '.' -> currentBuffer.GetChar() |> printf "%c"
     | _ -> printfn "not implimented %c" command
 
-    idx + 1
+let rec exec (text : string) =
+    let mutable idx = 0
+    while idx < text.Length do
+        let c = text.[idx]
+        match c with
+        | '[' ->
+            if (currentBuffer.IsZero()) then
+                idx <- gotoClose text idx // error FS0003: This value is not a function and cannot be applied
+        | ']' ->
+            if (not (currentBuffer.IsZero())) then
+                idx <- gotoOpen text idx // error FS0003: This value is not a function and cannot be applied
+        | _ -> execCore c
+        idx <- idx + 1
 
 [<EntryPoint>]
 let main argv =
-    text <- ReadFile argv.[0]
-    while idx < text.Length do
-        idx <- exec text.[idx]
-
+    ReadFile argv.[0] |> exec
     printfn ""
     0
 
